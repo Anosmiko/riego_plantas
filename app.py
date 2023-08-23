1f8f221bd54a87c7810b9eff2a45db7a9a32208e
NC <psi.nicolas.coddou@gmail.com>
2023-08-22 22:10:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ Prueba
 """)
 
 
-# sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
-# url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
-# df = pd.read_csv(url, usecols=[ 'Ingresar Planta', 'Ingresar Fecha', 'Estado de Humedad', '¿Se rego?', '¿Fertilización?'])
-# df = df.rename(columns={'Ingresar Planta': 'Planta',
-#                         'Ingresar Fecha': "Fecha",
-#                         'Estado de Humedad': 'Humedad',
-#                         '¿Se rego?': "Riego", 
-#                         '¿Fertilización?' : "Fertilización"})
-
-
+df = pd.read_csv(url, usecols=[ 'Ingresar Planta', 'Ingresar Fecha', 'Estado de Humedad', '¿Se rego?', '¿Fertilización?'])
+df = df.rename(columns={'Ingresar Planta': 'Planta',
+                        'Ingresar Fecha': "Fecha",
+                        'Estado de Humedad': 'Humedad',
+                        '¿Se rego?': "Riego", 
+                        '¿Fertilización?' : "Fertilización"})
 
 
 
-# st.dataframe(df) 
+
+
+st.dataframe(df) 
 
 
 
