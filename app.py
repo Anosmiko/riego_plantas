cb76ab275edb335b00030c47a99c804562a65b9d
NC <psi.nicolas.coddou@gmail.com>
2023-08-22 23:07:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import calendar
 import streamlit as st 
 
-### LECTURA CSV FORMULARIO
+############# LECTURA CSV FORMULARIO
 sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
@@ -36,19 +36,22 @@ seleccion_ultimo_riego = st.selectbox(
 filtered_df = df[(df["Planta"] == seleccion_ultimo_riego) & (df["Riego"] == "Si")]
 
 primera_fila = filtered_df.sort_values(by="Fecha").head(1)
-first_fecha_value = primera_fila["Fecha"].iloc[0]
-first_fecha_value = pd.to_datetime(first_fecha_value)
+ultima_fecha = pd.to_datetime(primera_fila["Fecha"].iloc[0])
+ultima_fecha = ultima_fecha.date()
 
-año = first_fecha_value.year
-mes = first_fecha_value.month
-dia = first_fecha_value.day
+dias_desde_ult_riego = (hoy - ultima_fecha).days
 
-st.write(hoy - first_fecha_value).days()
+st.write(f"El ultimo riego de la {seleccion_ultimo_riego} fue eñ {ultima_fecha}, hace {dias_desde_ult_riego} dias.")
+# año = first_fecha_value.year
+# mes = first_fecha_value.month
+# dia = first_fecha_value.day
+
+# st.write(hoy - first_fecha_value).days()
 
 #finds the first occurrence of today's date and changes its color 
 
 
-st.write(f"{año} {mes} {dia}")
+# st.write(f"{año} {mes} {dia}")
 
 
 
