41032e7c29302d37ee8bea93aa80be22a7116f97
NC <psi.nicolas.coddou@gmail.com>
2023-08-23 16:56:21 -0400

    Create alertas.py

---
+++ pages/alertas.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import streamlit as st 
+
+# LECTURA CSV FORMULARIO
+# =============================================================================
+sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+
+df = pd.read_csv(url, usecols=[ 'Ingresar Planta', 'Ingresar Fecha', 'Estado de Humedad', '¿Se rego?', '¿Fertilización?'])
+df = df.rename(columns={'Ingresar Planta': 'Planta',
+                        'Ingresar Fecha': "Fecha",
+                        'Estado de Humedad': 'Humedad',
+                        '¿Se rego?': "Riego", 
+                        '¿Fertilización?' : "Fertilización"})
+
+# =============================================================================
+
+
+# CHEQUEO PLANTAS BAJA HUMEDAD
+# =============================================================================
+ # Titulo
+st.header("Revisar")
+ 
+plantas_unique = df["Planta"].unique()
+
+for planta in plantas_unique:
+    ultima_inspeccion = df[df["Planta"] == planta].sort_values(by="Fecha").head(1)
+    
+    ultima_inspeccion_hum = ultima_inspeccion["Humedad"].iloc[0]
+    ultima_inspeccion_rieg = ultima_inspeccion["Riego"].iloc[0]
+
+
+    if  (ultima_inspeccion_hum <= 2) and (ultima_inspeccion_rieg == "No"):
+        st.caption(f"\u2757 {planta}")
+# =============================================================================
+
