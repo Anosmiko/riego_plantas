acf28ee42502a62150ecc92c3d8f94d09418cded
NC <psi.nicolas.coddou@gmail.com>
2023-08-22 22:15:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ import pandas as pd
 import requests
 import streamlit as st 
 
-st.write("""
-# Prueba AAAAAAAAAAAAAA
-
-Prueba
-""")
-
-
+### LECTURA CSV FORMULARIO
 sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
@@ -20,10 +14,15 @@ df = df.rename(columns={'Ingresar Planta': 'Planta',
                         '¿Fertilización?' : "Fertilización"})
 
 
+### Vista Ultimo Riego por planta
+# Create a form for users to input data for the new row
+seleccion_ultimo_riego = st.selectbox(
+                                "Seleccionar Planta:",
+                                options=df["Planta"].unique(),
+                                # index=0  # Set the default index to select the first city
+                            )
 
-
-
-st.dataframe(df) 
+st.dataframe(df[seleccion_ultimo_riego]) 
 
 
 
