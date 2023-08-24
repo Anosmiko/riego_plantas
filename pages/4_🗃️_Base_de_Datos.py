5c141b99c4168082edd5cdf9e9f90d6c721c90bf
NC <psi.nicolas.coddou@gmail.com>
2023-08-23 23:37:53 -0400

    Update 4_🗃️_Base_de_Datos.py

--- pages/4_🗃️_Base_de_Datos.py
+++ pages/4_🗃️_Base_de_Datos.py
@@ -15,15 +15,19 @@ st.title("Base de Datos")
 sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
-df = df.rename(columns={'Ingresar Planta': 'Planta',
+df = pd.read_csv(url)
+
+
+df = df.rename(columns={'Marca temporal': "Fecha Ingreso",
+                        'Ingresar Planta': 'Planta',
                         'Ingresar Fecha': "Fecha",
                         'Estado de Humedad': 'Humedad',
                         '¿Se rego?': "Riego", 
                         '¿Fertilización?' : "Fertilización"})
 
-#df = df.sort_values(by="Fecha", ascending=False)
-#df.reset_index(inplace=True)
-#df.drop("index", axis=1, inplace=True)
+df = df.sort_values(by="Fecha Ingreso", ascending=False)
+df.reset_index(inplace=True)
+df.drop("index", axis=1, inplace=True)
 
 
 # =============================================================================
