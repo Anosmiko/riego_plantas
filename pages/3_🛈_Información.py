e0689767f860b39809b52e3d7d8e1b5722ef9240
NC <psi.nicolas.coddou@gmail.com>
2023-08-23 23:32:18 -0400

    commit

--- pages/3_🛈_Información.py
+++ pages/3_🛈_Información.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import calendar
+
 
 st.set_page_config(page_title="Riego y Fertilizacion de Plantas",
                     page_icon= "🍃")
