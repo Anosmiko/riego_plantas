69dc8af6c092354462630894c2aff309646f3c39
NC <psi.nicolas.coddou@gmail.com>
2023-08-23 23:31:32 -0400

    Create 4_🗃️_Base_de_Datos.py

---
+++ pages/4_🗃️_Base_de_Datos.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import calendar
+
+st.set_page_config(page_title="Riego y Fertilizacion de Plantas",
+                    page_icon= "🍃")
+
+st.title("Base de Datos")
+
+
+
+# LECTURA CSV FORMULARIO
+# =============================================================================
+sheet_id = '1JcBYuaxBlGmuHVyi7FSaKChs8a49yeWWLA6SeZOEpXk'
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+
+df = pd.read_csv(url, usecols=[ 'Ingresar Planta', 'Ingresar Fecha', 'Estado de Humedad', '¿Se rego?', '¿Fertilización?'])
+df = df.rename(columns={'Ingresar Planta': 'Planta',
+                        'Ingresar Fecha': "Fecha",
+                        'Estado de Humedad': 'Humedad',
+                        '¿Se rego?': "Riego", 
+                        '¿Fertilización?' : "Fertilización"})
+
+
+# =============================================================================
+
+st.dataframe(df)
